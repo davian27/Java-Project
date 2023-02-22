e886637849c080cf6c4b412e50af8a20a8f7a736
davian <daviannaufalfadhil@gmail.com>
2023-02-22 16:22:28 +0700

    Updated 22 Februari 2023

---
+++ RPL_SEM2_Project/Materi_22_Februari_2023.java
@@ -0,0 +1,15 @@
+package RPL_SEM2_Project;
+
+public class Materi_22_Februari_2023 {
+
+  static void salam(String nama){
+    System.out.println("Halo "+nama);
+  }
+public static void main(String[]args){
+salam("Subari");
+salam("Wakri");
+salam("Kasim");
+salam("Sanusi");
+
+}
+}
