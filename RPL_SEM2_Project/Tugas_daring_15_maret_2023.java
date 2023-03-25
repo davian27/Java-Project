f56b6dc338b4db1d660dc66437fb4bc6379bd17d
davian27 <daviannaufalfadhil@gmail.com>
2023-03-25 14:04:19 +0700

    Updated 25 Maret 2023

---
+++ RPL_SEM2_Project/Tugas_daring_15_maret_2023.java
@@ -0,0 +1,13 @@
+package RPL_SEM2_Project;
+
+public class  Tugas_daring_15_maret_2023{
+
+  static void perkalian(int angka_1,int angka_2){
+   int hasil = angka_1 * angka_2;
+   System.out.println(hasil);
+
+  }
+  public static void main(String[]args){
+    perkalian(10,2);
+  }
+}
